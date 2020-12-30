6ed67312af6b01bed9f66870059c96f15c3a3072
MaiwennLeCacheux <74050437+MaiwennLeCacheux@users.noreply.github.com>
2020-12-30 12:28:10 +0100

    'fini'

---
+++ src/main/java/fr/ensim/tp5/controller/FragmentsController.java
@@ -0,0 +1,28 @@
+package fr.ensim.tp5.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FragmentsController {
+
+    /*@GetMapping("/fragments")
+    public String getHome() {
+        return "fragments.html";
+    }
+
+    @GetMapping("/markup")
+    public String markupPage() {
+        return "markup.html";
+    }
+
+    @GetMapping("/params")
+    public String paramsPage() {
+        return "params.html";
+    }
+
+    @GetMapping("/other")
+    public String otherPage(Model model) {
+        model.addAttribute("data", StudentUtils.buildStudents());
+        return "other.html";
+    }*/
+}
